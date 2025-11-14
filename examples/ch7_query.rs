456753f34fd22a6fd58a837f363dbc0227ce9970
myclooe <994386508@qq.com>
2025-11-14 09:28:43 +0800

    learn: query and query_param

---
+++ examples/ch7_query.rs
@@ -0,0 +1,252 @@
+//! 模拟一个玩家遭遇站的场景
+//! 4个敌人,4种类型
+//! boos 红色怪,绿色怪,隐形怪
+//! 3种攻击方式
+//! (B)omb 爆炸 Boss 以外都扣血
+//! (L)ash 鞭打 除了Player 和隐形怪都扣血
+//! (G)lare 所有敌人强制 BodyColor::White (隐身怪不在隐身)
+
+//! system pipe
+//! Query 用法 QueryData 与QueryFilter
+//! Query 的延伸用法 Single 和 Populated 和 Option<Single>
+
+use bevy::{ecs::query, prelude::*};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum QueryError {
+    #[error("none")]
+    None,
+}
+#[derive(Debug, Resource)]
+struct NotifyPlayerTimer(Timer);
+
+#[derive(Resource, Debug)]
+struct NotifyEnemiesTimer(Timer);
+
+// 玩家
+#[derive(Component)]
+struct Player;
+
+// boss
+#[derive(Component, Debug)]
+struct Boss;
+
+// 敌人
+#[derive(Component, Debug)]
+struct Enemy;
+
+// 生命值
+#[derive(Component, Deref, DerefMut, Debug)]
+struct Health(i64);
+
+// 护甲
+#[derive(Component, Debug)]
+struct Armor;
+
+// 颜色(是否隐身)
+#[derive(Component, Debug)]
+enum BodyColor {
+    Red,
+    Green,
+    White,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(NotifyPlayerTimer(Timer::from_seconds(
+            5.0,
+            TimerMode::Repeating,
+        )))
+        .insert_resource(NotifyEnemiesTimer(Timer::from_seconds(
+            5.0,
+            TimerMode::Repeating,
+        )))
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            choice
+                .pipe(bomb_all)
+                .pipe(lash_enemy)
+                .pipe(glare_all)
+                .map(in_choice_map),
+        )
+        .add_systems(Update, refresh_all)
+        .add_systems(Update, (notify_player, notify_enemies).chain())
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Player, Health(100), Armor));
+    commands.spawn((Enemy, Health(30), BodyColor::Green));
+    commands.spawn((Enemy, Health(1)));
+    commands.spawn((Boss, BodyColor::Red, Armor, Health(100)));
+}
+
+// 等待用户输入
+fn choice(input: Res<ButtonInput<KeyCode>>) -> Result<KeyCode, QueryError> {
+    let Some(code) = input.get_just_pressed().next() else {
+        return Err(QueryError::None);
+    };
+    Ok(*code)
+}
+// 丢炸弹, 场景中的所有生物都要扣血
+// 玩家和怪物受到伤害不同,需要分别处理(两个query)
+fn bomb_all(
+    In(key): In<Result<KeyCode, QueryError>>,
+    mut player_query: Query<&mut Health, (With<Player>, Without<Enemy>)>,
+    mut enemy_query: Query<&mut Health, (With<Enemy>, Without<Player>)>,
+) -> Result<KeyCode, QueryError> {
+    let Ok(code) = key else {
+        return Err(QueryError::None);
+    };
+
+    if code == KeyCode::KeyB {
+        let bomb_damage = 60;
+
+        for mut health in player_query.iter_mut() {
+            **health -= bomb_damage / 2;
+        }
+
+        for mut health in enemy_query.iter_mut() {
+            **health -= bomb_damage;
+        }
+    }
+    key
+}
+
+// # lash 除了玩家和隐身怪都扣血
+//  (Without<Player>, With<BodyColor>) 这个可以过滤玩家和隐身怪
+fn lash_enemy(
+    In(key): In<Result<KeyCode, QueryError>>,
+    mut query: Query<&mut Health, (Without<Player>, With<BodyColor>)>,
+) -> Result<KeyCode, QueryError> {
+    let Ok(code) = key else {
+        return Err(QueryError::None);
+    };
+
+    if code == KeyCode::KeyL {
+        let lash_damage = 10;
+
+        for mut health in query.iter_mut() {
+            **health -= lash_damage;
+        }
+    }
+
+    key
+}
+
+//
+fn glare_all(
+    In(key): In<Result<KeyCode, QueryError>>,
+    mut query: Query<(Option<&mut BodyColor>, Entity), Without<Player>>,
+    mut commands: Commands,
+) -> Result<KeyCode, QueryError> {
+    let Ok(code) = key else {
+        return Err(QueryError::None);
+    };
+    if code == KeyCode::KeyG {
+        for (bg, entity) in &mut query {
+            match bg {
+                Some(mut bc) => {
+                    *bc = BodyColor::White;
+                }
+                None => {
+                    commands.entity(entity).insert(BodyColor::White);
+                }
+            }
+        }
+    }
+    key
+}
+
+// 打印用户输入
+// map 只能连接函数,不能是system
+fn in_choice_map(key: Result<KeyCode, QueryError>) {
+    if let Ok(code) = key {
+        println!("map show: ({:?}\n\n)", code)
+    }
+}
+
+// 移出 health <=0的对象
+fn refresh_all(mut commands: Commands, query: Query<(Entity, &Health), With<Health>>) {
+    for (entity, health) in &query {
+        if **health <= 0 {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+// 当玩家不存在后,Single无法执行,并且会panic
+fn notify_player(
+    time: Res<Time>,
+    mut timer: ResMut<NotifyPlayerTimer>,
+    player: Option<Single<EntityRef, With<Player>>>, // 获取entity 的组件
+) {
+    timer.0.tick(time.delta());
+    // 不足自定义的时间,就什么都不做
+    if !timer.0.just_finished() {
+        return;
+    }
+    let Some(player) = player else {
+        println!("no player alive.");
+        return;
+    };
+
+    println!("---------------------------");
+    println!("(B)omb -60 (L)ash -10 (G)lare");
+
+    let Some(health) = player.get::<Health>() else {
+        return;
+    };
+
+    let Some(armor) = player.get::<Armor>() else {
+        return;
+    };
+
+    println!(
+        "player ({}): health:{:?} armor:{:?}",
+        player.id(),
+        health,
+        armor
+    );
+}
+fn notify_enemies(
+    time: Res<Time>,
+    mut timer: ResMut<NotifyEnemiesTimer>,
+    enemies: Populated<
+        (
+            Entity,
+            &Health,
+            Option<&Armor>,
+            Option<&BodyColor>,
+            Option<&Boss>,
+            Option<&Enemy>,
+        ),
+        Without<Player>,
+    >,
+) {
+    timer.0.tick(time.delta());
+    if !timer.0.just_finished() {
+        return;
+    }
+
+    for (entity, health, armor, bg, boss, enemy) in enemies {
+        print!("entity:{}", entity);
+        print!(" ({})", **health);
+        print!(" / ({:?})", armor);
+        if let Some(bg) = bg {
+            print!(" / {bg:?}")
+        }
+
+        if boss.is_some() {
+            print!(" / boss")
+        }
+        if enemy.is_some() {
+            print!(" / enemy")
+        }
+        println!()
+    }
+    println!()
+}
---
+++ examples/ch8_custiom_query_param.rs
@@ -0,0 +1,192 @@
+//! [`QueryData`]该宏允许定义自定义的查询和筛选类型。
+//! 在大多数简单场景中，常规的元组查询表现得非常出色。然而，使用以命名结构体形式声明的自定义查询则能带来以下优势：
+//！ - 它们有助于避免解构操作或使用“q.0, q.1, ...”这样的访问模式。
+//！ - 通过使用结构体来添加、删除组件或改变元素顺序，能极大地减轻维护负担，因为您无需更新解构元组的语句、无需关注元素的顺序等。相反，您只需添加或删除某个元素被使用的位置即可。
+//！ - 命名结构体支持组合模式，这使得查询类型更易于复用。
+//！ - 您可以绕过查询元组中 15 个组件的限制。//!
+
+use bevy::{
+    ecs::query::{self, QueryData, QueryFilter},
+    prelude::*,
+};
+use std::fmt::Debug;
+
+#[derive(Debug, Component)]
+struct ComponentA;
+#[derive(Debug, Component)]
+struct ComponentB;
+#[derive(Debug, Component)]
+struct ComponentC;
+#[derive(Debug, Component)]
+struct ComponentD;
+#[derive(Debug, Component)]
+struct ComponentZ;
+
+// QueryData 的目的是集成查询结果到一个自定义的结构体
+// 所以如果Query 中的Entity 不包含(非Option集成)某个Component 时就不会有结果,该System 就不会被执行
+#[derive(QueryData)]
+#[query_data(derive(Debug))]
+struct ReadOnlyCustomQuery<T: Component + Debug, P: Component + Debug> {
+    entity: Entity,
+    a: &'static ComponentA,
+    b: Option<&'static ComponentB>,
+    nested: NestedQuery,
+    optional_nested: Option<NestedQuery>,
+    optional_tuple: Option<(&'static ComponentB, &'static ComponentZ)>,
+    z: Option<&'static ComponentZ>, // 因为z 没有加入
+    generic: GenericQuery<T, P>,
+    enpty: EmptyQuery,
+}
+
+#[derive(QueryData)]
+#[query_data(derive(Debug))]
+struct NestedQuery {
+    c: &'static ComponentC,
+    d: Option<&'static ComponentD>,
+}
+
+#[derive(QueryData)]
+#[query_data(derive(Debug))]
+struct GenericQuery<T: Component, P: Component> {
+    generic: (&'static T, &'static P),
+}
+
+#[derive(QueryData)]
+#[query_data(derive(Debug))]
+struct EmptyQuery {
+    empt: (),
+}
+
+// 所有的结构属性都是and 联合,所以一旦有一个条件不满足,将没有结果
+#[derive(QueryFilter)]
+struct CustomQueryFilter<T: Component, P: Component> {
+    _c: With<ComponentC>,
+    _d: With<ComponentD>,
+    // 如果不将 Added 加入到Or 条件,那么query就会得到一次结果
+    _or: Or<(Added<ComponentC>, Changed<ComponentD>, Without<ComponentZ>)>,
+    _generic_tuple: (With<T>, With<P>),
+    // 满足不了CompoenntZ 条件
+    // _generic_tuple2:(With<T>,With<P>,With<ComponentZ>)
+}
+
+#[derive(QueryData)]
+#[query_data(mutable, derive(Debug))]
+struct CustomQuery<T: Component + Debug, P: Component + Debug> {
+    entity: Entity,
+    a: &'static mut ComponentA,
+    b: Option<&'static mut ComponentB>,
+    nested: NestedQuery,
+    optional_nested: Option<NestedQuery>,
+    optional_tuple: Option<(NestedQuery, &'static mut ComponentZ)>,
+    generic: GenericQuery<T, P>,
+    empty: EmptyQuery,
+}
+
+type NestedTupleQuery<'w> = (&'w ComponentC, &'w ComponentD);
+type GenericTupleQuery<'w, T, P> = (&'w T, &'w P);
+
+fn main() {
+    App::new()
+        .add_systems(Startup, spawn)
+        .add_systems(
+            Update,
+            (
+                print_components_read_only,
+                print_components_iter_mut,
+                print_components_iter,
+                print_components_tuple,
+            )
+                .chain(),
+        )
+        .run();
+}
+
+fn spawn(mut commands: Commands) {
+    // z 不被加入
+    commands.spawn((ComponentA, ComponentB, ComponentC, ComponentD));
+}
+
+// 只读的查询
+fn print_components_read_only(
+    query: Query<
+        ReadOnlyCustomQuery<ComponentC, ComponentD>,
+        CustomQueryFilter<ComponentC, ComponentD>,
+    >,
+) {
+    println!("print components (read_only)");
+    for e in &query {
+        println!("Entity: {:?}", e.entity);
+        println!("A: {:?}", e.a);
+        println!("B: {:?}", e.b);
+        println!("Nested: {:?}", e.nested);
+        println!("Optional nested: {:?}", e.optional_nested);
+        println!("Optional tuple: {:?}", e.optional_tuple);
+        println!("Generic: {:?}", e.generic);
+        println!("enpty: {:?}", e.enpty);
+    }
+    println!()
+}
+
+// 当使用可写的版本时,需要两个地方加mut
+fn print_components_iter_mut(
+    mut query: Query<
+        CustomQuery<ComponentC, ComponentD>,
+        CustomQueryFilter<ComponentC, ComponentD>,
+    >,
+) {
+    println!("print components (iter mut)");
+    for e in &mut query {
+        let e: CustomQueryItem<'_, '_, _, _> = e;
+        println!("Entity: {:?}", e.entity);
+        println!("A: {:?}", e.a);
+        println!("B: {:?}", e.b);
+        println!("Nested: {:?}", e.nested);
+        println!("Optional nested: {:?}", e.optional_nested);
+        println!("Optional tuple: {:?}", e.optional_tuple);
+        println!("Generic: {:?}", e.generic);
+    }
+}
+//  这是只读版本
+fn print_components_iter(
+    query: Query<CustomQuery<ComponentC, ComponentD>, CustomQueryFilter<ComponentC, ComponentD>>,
+) {
+    println!("print components (iter mut)");
+    for e in &query {
+        let e: CustomQueryReadOnlyItem<'_, '_, _, _> = e;
+        println!("Entity: {:?}", e.entity);
+        println!("A: {:?}", e.a);
+        println!("B: {:?}", e.b);
+        println!("Nested: {:?}", e.nested);
+        println!("Optional nested: {:?}", e.optional_nested);
+        println!("Optional tuple: {:?}", e.optional_tuple);
+        println!("Generic: {:?}", e.generic);
+    }
+}
+// 这是一个常规传统的方式
+// 复杂度集中到了参数重,不易阅读
+fn print_components_tuple(
+    query: Query<
+        (
+            Entity,
+            &ComponentA,
+            &ComponentB,
+            NestedTupleQuery,
+            GenericTupleQuery<ComponentC, ComponentD>,
+        ),
+        (
+            With<ComponentC>,
+            With<ComponentD>,
+            Or<(Added<ComponentC>, Changed<ComponentD>, Without<ComponentZ>)>,
+        ),
+    >,
+) {
+    println!("Print components (tuple):");
+    for (entity, a, b, nested, (generic_c, generic_d)) in &query {
+        println!("Entity: {:?}", entity);
+        println!("A: {:?}", a);
+        println!("B: {:?}", b);
+        println!("Nested: {:?}", nested);
+        println!("generic_c {:?}", generic_c);
+        println!("generic_d {:?}", generic_d);
+    }
+}
